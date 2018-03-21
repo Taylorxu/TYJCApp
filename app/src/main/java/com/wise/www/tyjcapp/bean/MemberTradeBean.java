3733f4878dc55583ea02dbc1ff8524f618397386
Taylorxu <zhiguang_xu@live.cn>
2018-03-21 16:54:26 +0800

    交易量

---
+++ app/src/main/java/com/wise/www/tyjcapp/bean/MemberTradeBean.java
@@ -0,0 +1,26 @@
+package com.wise.www.tyjcapp.bean;
+
+/**
+ * Created by Administrator on 2018/3/21.
+ */
+
+public class MemberTradeBean {
+    String membername;
+    String tradeNumber;
+
+    public String getMembername() {
+        return membername;
+    }
+
+    public void setMembername(String membername) {
+        this.membername = membername;
+    }
+
+    public String getTradeNumber() {
+        return tradeNumber;
+    }
+
+    public void setTradeNumber(String tradeNumber) {
+        this.tradeNumber = tradeNumber;
+    }
+}
--- app/src/main/java/com/wise/www/tyjcapp/main/SecondFragment.java
+++ app/src/main/java/com/wise/www/tyjcapp/main/SecondFragment.java
@@ -1,22 +1,34 @@
 package com.wise.www.tyjcapp.main;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.wise.www.tyjcapp.BR;
 import com.wise.www.tyjcapp.R;
+import com.wise.www.tyjcapp.adapter.XAdapter;
+import com.wise.www.tyjcapp.adapter.XViewHolder;
+import com.wise.www.tyjcapp.bean.MemberTradeBean;
+import com.wise.www.tyjcapp.bean.SystemCaseBean;
 import com.wise.www.tyjcapp.databinding.FragmentSecondBinding;
+import com.wise.www.tyjcapp.databinding.ItemMemberTradeTopBinding;
 
+import java.util.ArrayList;
 
 
 public class SecondFragment extends Fragment {
     FragmentSecondBinding binding;
+    private ArrayList<MemberTradeBean> list;
+    private int[] colorspan;
 
     public SecondFragment() {
 
@@ -28,6 +40,12 @@ public class SecondFragment extends Fragment {
     }
 
     @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        colorspan = new int[]{getResources().getColor(R.color.color_top1_b), getResources().getColor(R.color.color_top2_b), getResources().getColor(R.color.color_top3_b)};
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false);
@@ -36,22 +54,55 @@ public class SecondFragment extends Fragment {
     }
 
 
+    XAdapter<MemberTradeBean, ItemMemberTradeTopBinding> adapter = new XAdapter.SimpleAdapter<MemberTradeBean, ItemMemberTradeTopBinding>(0, R.layout.item_member_trade_top) {
+
+        @Override
+        public void onBindViewHolder(XViewHolder<MemberTradeBean, ItemMemberTradeTopBinding> holder, int position) {
+            super.onBindViewHolder(holder, position);
+            if (position == 0 || position == 1 || position == 2) {
+                holder.getBinding().textNo.setTextColor(Color.RED);
+                holder.getBinding().textTradeNo.setTextColor(Color.RED);
+                holder.getBinding().layoutItem.setBackgroundColor(colorspan[position]);
+            }
+            String No = String.valueOf(position + 1);
+            if (position < 9) No = "0" + No;
+            holder.getBinding().textNo.setText(No.toString());
+
+            holder.getBinding().textMemberName.setText(getItemData(position).getMembername());
+            holder.getBinding().textTradeNo.setText(getItemData(position).getTradeNumber());
+        }
+    };
+
     @SuppressLint("ResourceAsColor")
     private void initView() {
+        crea();
         binding.title.setText(R.string.str_top);
-      /*  binding.refreshLayout.setColorSchemeColors(getResources().getColor(R.color.colorPrimary));
+        binding.refreshLayout.setColorSchemeColors(getResources().getColor(R.color.colorPrimary));
         binding.refreshLayout.setOnRefreshListener(refreshListener);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         binding.contentAnnounceList.setLayoutManager(layoutManager);
-        binding.contentAnnounceList.setAdapter(adapter);*/
+        binding.contentAnnounceList.addItemDecoration(new DividerItemDecoration(getContext(), LinearLayoutManager.VERTICAL));
+        binding.contentAnnounceList.setAdapter(adapter);
+        adapter.setList(list);
 
+    }
+
+    private void crea() {
+        binding.refreshLayout.setRefreshing(true);
+        list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            MemberTradeBean tradeBean = new MemberTradeBean();
+            tradeBean.setMembername("网银系统啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊" + i);
+            tradeBean.setTradeNumber("10000" + i);
+            list.add(tradeBean);
+        }
+        binding.refreshLayout.setRefreshing(false);
     }
 
     SwipeRefreshLayout.OnRefreshListener refreshListener = new SwipeRefreshLayout.OnRefreshListener() {
         @Override
         public void onRefresh() {
-
-
+            crea();
         }
     };
 
